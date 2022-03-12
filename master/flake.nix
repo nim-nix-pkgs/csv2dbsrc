8dfaac12922dcd1d83d359bd23d2e0a721676ad6
hugosenari <hugosenari@gmail.com>
2022-03-12 04:15:40 -0300

    chore: re index csv2dbsrc

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''create db util sources from csv'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-csv2dbsrc-master.flake = false;
+  inputs.src-csv2dbsrc-master.owner = "z-kk";
+  inputs.src-csv2dbsrc-master.ref   = "refs/heads/master";
+  inputs.src-csv2dbsrc-master.repo  = "csv2dbsrc";
+  inputs.src-csv2dbsrc-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-csv2dbsrc-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-csv2dbsrc-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
---
+++ v0_2_1/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''create db util sources from csv'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-csv2dbsrc-v0_2_1.flake = false;
+  inputs.src-csv2dbsrc-v0_2_1.owner = "z-kk";
+  inputs.src-csv2dbsrc-v0_2_1.ref   = "refs/tags/v0.2.1";
+  inputs.src-csv2dbsrc-v0_2_1.repo  = "csv2dbsrc";
+  inputs.src-csv2dbsrc-v0_2_1.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-csv2dbsrc-v0_2_1"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-csv2dbsrc-v0_2_1";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
