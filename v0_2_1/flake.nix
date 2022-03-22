a96915c00bd26a5694979be18797530115879941
hugosenari <hugosenari@gmail.com>
2022-03-22 00:27:00 -0300

    chore: re-index ../nimpkgs/c/csv2dbsrc/v0_2_1

--- v0_2_1/flake.nix
+++ v0_2_1/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-csv2dbsrc-v0_2_1.flake = false;
   inputs.src-csv2dbsrc-v0_2_1.owner = "z-kk";
-  inputs.src-csv2dbsrc-v0_2_1.ref   = "refs/tags/v0.2.1";
+  inputs.src-csv2dbsrc-v0_2_1.ref   = "v0_2_1";
   inputs.src-csv2dbsrc-v0_2_1.repo  = "csv2dbsrc";
   inputs.src-csv2dbsrc-v0_2_1.type  = "github";
   
